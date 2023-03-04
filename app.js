6d3ef12c39feed58a7c9e519163432bb37f09ade
atilgner <atilgner@digital-strata.com>
2023-03-03 18:29:24 -0800

    fix: not socket mode

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ config();
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
-  socketMode: true,
   appToken: process.env.SLACK_APP_TOKEN,
 });
 
